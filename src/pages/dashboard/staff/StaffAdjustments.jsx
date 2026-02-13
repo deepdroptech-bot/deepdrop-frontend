4993f26bc17bc7ef8a56d7cfc6766f8e7eb476df
lacock32-a11y <lacock32@gmail.com>
2026-02-13 12:01:37 +0000

    import changes

--- src/pages/dashboard/staff/StaffAdjustments.jsx
+++ src/pages/dashboard/staff/StaffAdjustments.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { staffAPI } from "../../../services/staffService";
-import { useAuth } from "../../context/AuthContext";
+import { useAuth } from "../../../context/AuthContext";
 
 export default function StaffAdjustments({ staff }) {
   const { user } = useAuth();
